8a61c8ea68ab8fa0d74aeb436f5c4e4060960ec0
Mownika2004 <112697242+Mownika2004@users.noreply.github.com>
2022-09-19 11:22:31 +0530

    Add files via upload

---
+++ program.03 cpp.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<bits/stdc++.h> 
+using namespace std;
+
+int main() {
+  
+  string greeting = "!@#$%";
+  reverse(greeting.begin(),greeting.end());
+  cout<<greeting<<endl;
+}
+
+
+
